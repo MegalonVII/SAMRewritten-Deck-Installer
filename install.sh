1b2a03e850cbf0c12a689a95631282f3243d8c94
Megalon <megalonsba@gmail.com>
2025-04-28 18:49:51 -0400

    that worked but how about this move?

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+# stages directories
 mkdir -p ~/.local/share/applications
 mkdir -p ~/.local/share/icons
 mkdir -p ~/Applications
 
+# downloads appimage
 mkdir -p ~/.megalonvii
 cd ~/.megalonvii
 wget -O SAMRewritten.zip https://github.com/PaulCombal/SamRewritten/files/10149585/SamRewritten-b50966a-x86_64.zip
 unzip -o SAMRewritten.zip
 rm SAMRewritten.zip
+mv SamRewritten-b5096a-x86_64.AppImage ~/Applications/SAMRewritten.AppImage
