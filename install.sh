08f9fe362418a8eb764d4b5b3d43e986ded5b0d1
Megalon <megalonsba@gmail.com>
2025-04-28 19:06:32 -0400

    this may be the end of the installer?

--- install.sh
+++ install.sh
@@ -6,10 +6,25 @@ mkdir -p ~/.local/share/applications
 mkdir -p ~/.local/share/icons
 mkdir -p ~/Applications
 
-# downloads appimage
+# downloads appimage and places in the right place
 mkdir -p ~/.megalonvii
 cd ~/.megalonvii
 wget -O SAMRewritten.zip https://github.com/PaulCombal/SamRewritten/files/10149585/SamRewritten-b50966a-x86_64.zip
 unzip -o SAMRewritten.zip
 rm SAMRewritten.zip
 mv SamRewritten-b50966a-x86_64.AppImage ~/Applications/SAMRewritten.AppImage
+
+# curls extras as needed
+curl -L -o https://raw.githubusercontent.com/MegalonVII/SAMRewritten-Deck-Installer/main/extras/SAMRewritten.png
+mv SAMRewritten.png ~/.local/share/icons
+curl -L -o https://raw.githubusercontent.com/MegalonVII/SAMRewritten-Deck-Installer/main/extras/SAMRewritten.desktop
+cp SAMRewritten.desktop ~/Desktop
+mv SAMRewritten.desktop ~/.local/share/applications
+
+# finishing up
+cd ..
+rm -rf ~/.megalonvii
+rm -rf ~/Desktop/install.desktop
+echo
+echo "Done!"
+echo
